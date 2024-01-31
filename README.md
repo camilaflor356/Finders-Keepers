a202ab1d91e0e58ea8caad71cfa56ace2c119d41
Horace Mann <camila_florencio@horacemann.org>
2024-01-31 02:43:13 -0500

    pushing

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Finders-Keepers
+
+My idea is to make a website where you can search through a database of restaurants throughout the world. I get really interested in the cuisines and cultural differences in other countries and I thought this would be a cool way of seeing that information.
+
+# Objectives
+
+* Some front-end so that the user can choose what sections they want to search through (cuisine, currency, country etc.) and see the information on their screen. Vote for restaurants that are/seem good.
+* For backend I use a database of restaurant information. Then I would need to create a way for users to SEARCH through the database through the different categories. 
+* I need to learn how to display multiple rows from a database at once.
+* STRETCH GOAL if I have time, I will find a way to allow users to filter with more than one category at the same time.
+* I considered using a database about one sole restaurant and it's menu but I found it restricting.
+
+# Submission
+
+Credit for database: https://www.kaggle.com/datasets/shrutimehta/zomato-restaurants-data?resource=download&authuser=0
+
+Filter by cuisine
+localhost:4000/cuisines/:cuisines (GET endpoint)-- put in a cuisine to find restaurants that serve it.
+
+Filter by price rating
+localhost:4000/prices/:prices (GET endpoint)-- put in a price range (1-4, 4 being most expensive) to find restaurants within it.
+
+Filter by ratings
+localhost:4000/ratings/:ratings (GET endpoint)-- put in a rating (0-5, 5 being highest rating) to find restaurants with that rating.
+
+Other endpoints (not made)
+localhost:4000/country/:country (GET endpoint)-- put in a country to find restaurants in that country.
+
+localhost:4000/votes/:id (GET endpoint)-- enter id of a restaurant to find the votes of a restaurant.
+
+localhost:4000/voting (POST endpoint) -- vote for a restaurant.
---
+++ index.js
@@ -0,0 +1,129 @@
+//-----------------------------
+// #region Setup
+//-----------------------------
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+const path = require("path");
+const sqlite = require("sqlite3").verbose();
+
+//#endregion Setup
+
+//-----------------------------
+//#region App Config
+//-----------------------------
+// https://stackoverflow.com/questions/23259168/what-are-express-json-and-express-urlencoded
+// Middleware that parses POST / PUT requests from a client
+app.use(express.json());
+
+// Handle CORS w/ client
+// For more information about CORS (Cross-Origin Resource Sharing):
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+app.use((req, res, next) => {
+  // Allow access from multiple origins
+  const allowedOrigins = [
+    "http://localhost:8080",
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  // Allow specific requests
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Pass to next layer of middleware
+  next();
+});
+
+const dbFile = path.join(__dirname, "zomatoRest.db");
+const db = new sqlite.Database(dbFile, (error) => {
+  if (error) return console.error(error.message);
+  console.log(`Connected to database ${dbFile}`);
+});
+
+
+//begin endpoints//
+
+//only returns one row
+//0-5
+const filterByRatings = (request, response) => {
+    const ratings = parseInt(request.params.ratings);
+    const query = `SELECT * FROM restaurants WHERE "Aggregate rating" = ?`;
+   
+   
+    db.get(query, [ratings], (error, result) => {
+      if (error) {
+        console.error(error.message);
+        response.status(400).json({ error: error.message });
+        return;
+      }
+      if (result) {
+        response.json(result);
+      } else {
+        response.sendStatus(404);
+      }
+    });
+   };
+   
+app.get("/ratings/:ratings", filterByRatings);
+
+//only one row
+//1-4
+const filterByPrice = (request, response) => {
+    const prices = parseInt(request.params.prices);
+    const query = `SELECT * FROM restaurants WHERE "price range" = ?`;
+   
+   
+    db.get(query, [prices], (error, result) => {
+      if (error) {
+        console.error(error.message);
+        response.status(400).json({ error: error.message });
+        return;
+      }
+    
+      if (result) {
+        response.json(result);
+      } else {
+        response.sendStatus(404);
+      }
+    });
+   };
+
+app.get("/prices/:prices", filterByPrice);
+
+//not returning
+const filterByCuisines = (request, response) => {
+    const cuisines = parse(request.params.cuisines);
+    const query = `SELECT * FROM restaurants WHERE "cuisines" LIKE "%?%"`;
+   
+   
+    db.get(query, [cuisines], (error, result) => {
+      if (error) {
+        console.error(error.message);
+        response.status(400).json({ error: error.message });
+        return;
+      }
+
+      if (result) {
+        response.json(result);
+      } else {
+        response.sendStatus(404);
+      }
+    });
+   };
+   
+   app.get("/cuisines/:cuisines", filterByCuisines);
+//end endpoints//
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
